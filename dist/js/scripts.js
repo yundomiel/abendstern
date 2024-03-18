1932501e7c242472f96a7e7537f78c2b5046e159
sar.yoon89@gmail.com <sar.yoon89@gmail.com>
2024-03-18 23:45:32 +0100

    changed index, style and script

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -52,3 +52,63 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// JavaScript
+document.querySelector('.first-image').addEventListener('click', function() {
+    this.classList.add('hidden'); // 첫 번째 이미지 숨기기
+    document.querySelector('.second-image').classList.remove('hidden'); // 두 번째 이미지 표시
+});
+
+document.querySelector('.second-image').addEventListener('click', function() {
+    document.querySelector('.first-image').classList.remove('hidden'); // 첫 번째 이미지 표시
+    this.classList.add('hidden'); // 두 번째 이미지 숨기기
+});
+
+
+// JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+    var additionalInfo = document.querySelector('.additional-info');
+    var sentences = [
+      "Hi, I am an ISTQB certified Test Engineer.",
+      "My passion is in Test Automation, especially for Mobile Apps.",
+      "I am a self-taught tester and am keep improving my skills.",
+      "Undómiel means Evening Star, from <Lord of the Ring>.",
+      "It's the elf Arwen's name, my favorite character.",
+      "Her image embodies grace, determination and courage.",
+      "These align with my core values as a self-initiative IT professional.",
+      "I took unconventional path to come to Tech domain as a minority.",
+      "Still I love being the rare kind.",
+      "Just Undómiel's commitment, I want to pursue my career. ",
+      "Wanna know more about me?",
+      "Find me @yundomiel on Github."
+    ];
+  
+    function typeWriter(text, i, cb) {
+      if (i < text.length) {
+        additionalInfo.innerHTML += text.charAt(i);
+        setTimeout(function() {
+          typeWriter(text, i + 1, cb);
+        }, 100); // 0.1초마다 한 글자씩 추가
+      } else {
+        setTimeout(cb, 3000); // 3초 후에 텍스트 사라짐
+      }
+    }
+  
+    function animateText() {
+      sentences.forEach(function(sentence, index) {
+        setTimeout(function() {
+          additionalInfo.innerHTML = '';
+          typeWriter(sentence, 0, function() {
+            setTimeout(function() {
+              additionalInfo.innerHTML = '';
+            }, 5000); // 5초 후에 텍스트 사라짐
+          });
+        }, index * 11000); // 11초 간격으로 문장을 나타나게 설정 (타이핑 시간 100ms * 글자 수 + 3초)
+      });
+    }
+  
+    animateText(); // 처음 애니메이션 실행
+  
+    setInterval(animateText, sentences.length * 11000); // 애니메이션 반복
+  });
+  
